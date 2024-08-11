bd5f1284346302ac429d2d3e8efc4305c5d8ecd3
Samuel <samlarivs@outlook.com>
2024-08-11 12:53:49 -0400

    react practice with alex

---
+++ Brocode-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+return(
+    <div className="card">
+        <img className="card-image" src="https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-1/376870350_10222908174724110_2992553723846249625_n.jpg?stp=dst-jpg_p480x480&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=G2VqT9f4Y7sQ7kNvgGlVAKL&_nc_ht=scontent-bos5-1.xx&oh=00_AYApBUVdbKel2myScfHvxv1tvjVrezV0WX5A0nMToBMdqA&oe=66BEC6A0" alt="profile picture"></img>
+        <h2 className="card-title">Sammy Bones</h2>
+        <p className="card-text">I make pancakes for Alex and I need new friends.</p>
+    </div>
+);
+}
+
+export default Card
