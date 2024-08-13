0fffa9e5f60d02bbd02b8c29a021992694edd4c7
Samuel <samlarivs@outlook.com>
2024-08-13 17:00:38 -0400

    fixed color display

--- Brocode-react-app/src/ColorPicker.jsx
+++ Brocode-react-app/src/ColorPicker.jsx
@@ -12,7 +12,7 @@ function ColorPicker(){
         <div className="Color-Picker-Container">
             <h1>Color Picker</h1>
             <div className = "color-display" style={{backgroundColor: color}}>
-                <p>Selected Color</p>
+                <p>Selected Color: {color}</p>
             </div>
             <label>Select a Color</label>
             <input type="color" value={color} onChange={handleColorChange} />
